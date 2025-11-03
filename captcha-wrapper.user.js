0eeaf2cff99a51580f43a0a754706385ec5c2e4b
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-03 01:52:39 +0000

    Create captcha-wrapper.user.js

---
+++ captcha-wrapper.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TeamRocket Captcha Discord and Telegram Notification with Timer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Notifies Discord/Telegram when a captcha appears and redirects after timer
+// @author       TeamRocket
+// @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
+// @include      **tribalwars**
+// @require      https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/captcha-notifier.js
+// @updateURL    https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/captcha-wrapper.user.js
+// @downloadURL  https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/captcha-wrapper.user.js
+// @grant        none
+// ==/UserScript==
